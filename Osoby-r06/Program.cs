4004a3b44dd230ae9d833a4a3479b5e97982137b
Krzysztof Wołowski <78325693+Krzysztof-bolt@users.noreply.github.com>
2024-07-02 15:54:29 +0200

    Dodaj pliki projektów.

---
+++ Osoby-r06/Program.cs
@@ -0,0 +1,40 @@
+﻿using BibliotekaWspolna;
+
+Osoba henryk = new()
+{
+    Nazwisko = "Henryk",
+    DataUrodzenia = new(year: 2001, month: 3, day: 25)
+};
+
+henryk.WypiszWKonsoli();
+
+//niegeneryczna kolekcja z wyszukiwaniem
+System.Collections.Hashtable wyszukiwanieObiektow = new();
+wyszukiwanieObiektow.Add(key: 1, value: "Alpha");
+wyszukiwanieObiektow.Add(key: 2, value: "Beta");
+wyszukiwanieObiektow.Add(key: 3, value: "Gamma");
+wyszukiwanieObiektow.Add(key: henryk, value: "Delta");
+
+int klucz = 2;
+WriteLine(format: "Klucz {0} ma wartosc : {1}",
+    arg0: klucz,
+    arg1: wyszukiwanieObiektow[klucz]);
+
+//szukamy wartosci o kluczu rownym zmiennej henryk
+WriteLine(format: "Klucz {0} ma wartosc : {1}",
+    arg0: henryk,
+    arg1: wyszukiwanieObiektow[henryk]);
+
+    //generyczna kolekcja wyszykująca
+Dictionary<int, string> slownikIntString = new();
+slownikIntString.Add(key: 1, value: "Alpha");
+slownikIntString.Add(key: 2, value: "Beta");
+slownikIntString.Add(key: 3, value: "Gamma");
+slownikIntString.Add(key: 4, value: "Delta");
+
+klucz = 3;
+
+WriteLine(format: "Klucz {0} ma wartosc : {1}",
+    arg0: klucz,
+    arg1: slownikIntString[klucz]);
+
